31a8bb863fcc8b640dcf7c47eaec38348d7ae2e0
tondi@@work <dinakartondikatti2@gmail.com>
2025-08-13 00:51:22 +0530

    adding all file into pomframework branch

---
+++ src/main/java/pom_class/GitCheckingUpadtes.java
@@ -0,0 +1,12 @@
+package pom_class;
+
+public abstract class GitCheckingUpadtes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is pom framework ");
+		
+	}
+
+}
